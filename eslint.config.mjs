885f6c92d1ec25408f2d02d53dfa6636aa616301
Matteo Geremia <mattegere93@gmail.com>
2025-09-23 00:56:16 +0200

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,10 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    rules: {
+      // Warning per l'uso di 'any'
+      '@typescript-eslint/no-explicit-any': 'off',
+      // Warning per l'uso di <img> invece di next/image
+      'next/no-img-element': 'off',
+    },
   },
 ];
 
